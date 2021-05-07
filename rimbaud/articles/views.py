f718a1cc8a384af9f0f4252a16eb44d7c594f966
NicolasM <nicolas.malgat@hotmail.fr>
2021-05-07 13:21:46 +0200

    Add filter by date for articles

--- rimbaud/articles/views.py
+++ rimbaud/articles/views.py
@@ -1,16 +1,23 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
+from datetime import datetime
 
 from .models import Article
 
 def index(request):
-    liste_articles = Article.objects.filter(visibility=True).order_by('-pub_date')
+    liste_articles = Article.objects.filter(
+        visibility=True,
+        pub_date__gt=datetime.now()
+    ).order_by('-pub_date')
 
     return render(request, 'articles/index.html', {'liste_articles': liste_articles})
 
 def detail(request, article_id):
-    article = get_object_or_404(Article, pk=article_id, visibility=True)
+    article = get_object_or_404(
+        Article, 
+        pk=article_id, 
+        visibility=True, 
+        pub_date__gt=datetime.now()
+    )
 
     return  render(request, 'articles/detail.html', {'article': article})
