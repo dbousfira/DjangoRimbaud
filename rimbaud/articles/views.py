e3cd84135debdee4a35c6d0240d4250d98b32d88
Djamel Bousfira <d.bousfira@hotmail.fr>
2021-05-07 11:50:15 +0200

    feat:  champs de recherche dans lequel l'utilisateur saisira l'ID

--- rimbaud/articles/views.py
+++ rimbaud/articles/views.py
@@ -1,17 +1,40 @@
 from django.http import HttpResponse
+from django.http.response import Http404, JsonResponse
+from django.template import loader
+
+from django.shortcuts import get_object_or_404, render
+
 
 from .models import Article
 
 
 def index(request):
     latest_article_list = Article.objects.order_by('-pub_date')[:5]
-    output = ', '.join([a.article for a in latest_article_list])
+    template = loader.get_template('articles/index.html')
+    context = {
+        'latest_article_list': latest_article_list,
+    }
 
-    return HttpResponse(output)
+    if request.is_ajax():
+        url_parameter = request.GET.get("q")
+
+        latest_article_list = Article.objects.all().filter(pk__startswith=url_parameter)
+
+        html = loader.render_to_string(
+            template_name="articles/_articles_results.html",
+            context={"latest_article_list": latest_article_list}
+        )
+
+        data_dict = {"html_from_view": html}
+
+        return JsonResponse(data=data_dict, safe=False)
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, article_id):
-    return HttpResponse("You're looking at article %s." % article_id)
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'articles/detail.html', {'article': article})
 
 
 def results(request, article_id):
